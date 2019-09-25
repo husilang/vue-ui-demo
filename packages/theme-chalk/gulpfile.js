1e7d18cf6f66478755238c922c6a27f338ed73e0
husilang <601906911@qq.com>
2019-09-25 11:15:28 +0800

    scss 自定义 bem

--- packages/theme-chalk/gulpfile.js
+++ packages/theme-chalk/gulpfile.js
@@ -1,28 +1,10 @@
 var {series, src, dest, watch, task} = require("gulp");
 var sass = require("gulp-sass");
 var bem = require("postcss-bem-fix");
-var nested = require("postcss-nested");
-var postcss = require("gulp-postcss");
 
 function compile() {
-    var processors = [
-
-        bem({
-            defaultNamespace: 'zm',
-            style: 'suit',
-            shortcuts: {
-                component: "b",
-                modifier: "m",
-                descendent: "e",
-                state: 'when',
-                utility: 'util'
-            }
-        }),
-        nested(),
-    ];
     return src("./src/*.scss")
         .pipe(sass.sync())
-        .pipe(postcss(processors))
         .pipe(dest("./lib"));
 }
 
